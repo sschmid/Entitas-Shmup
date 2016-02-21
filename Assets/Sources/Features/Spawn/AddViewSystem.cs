f6f04426fb61b02aacf8f275d544ddbdf030574f
Simon Schmid <mail@sschmid.com>
2016-02-21 17:36:13 +0100

    Added views container

--- Assets/Sources/Features/Spawn/AddViewSystem.cs
+++ Assets/Sources/Features/Spawn/AddViewSystem.cs
@@ -2,13 +2,20 @@ using System.Collections.Generic;
 using Entitas;
 using UnityEngine;
 
-public class AddViewSystem : IReactiveSystem {
+public class AddViewSystem : IReactiveSystem, ISetPool {
 
     public TriggerOnEvent trigger { get { return CoreMatcher.Resource.OnEntityAdded(); } }
+
+    Transform _container;
+
+    public void SetPool(Pool pool) {
+        _container = new GameObject(pool.metaData.poolName + " Views").transform;
+    }
 
     public void Execute(List<Entity> entities) {
         foreach (var e in entities) {
             var gameObject = Object.Instantiate(Resources.Load<GameObject>(e.resource.name));
+            gameObject.transform.SetParent(_container, false);
             e.AddView(gameObject);
         }
     }
