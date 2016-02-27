524b69dd6046fd10db4e85e1fba6c50f5fa3b768
Simon Schmid <mail@sschmid.com>
2016-02-27 18:56:44 +0100

    Fixed Feature

--- Assets/Sources/Features/Feature.cs
+++ Assets/Sources/Features/Feature.cs
@@ -1,11 +1,11 @@
 ﻿#if (!ENTITAS_DISABLE_VISUAL_DEBUGGING && UNITY_EDITOR)
-public class Feature : Entitas.Systems {
-    public Feature(string name) : base() {
+public class Feature : Entitas.Unity.VisualDebugging.DebugSystems {
+    public Feature(string name) : base(name) {
     }
 }
 #else
-public class Feature : Entitas.Unity.VisualDebugging.DebugSystems {
-    public Feature(string name) : base(name) {
+public class Feature : Entitas.Systems {
+    public Feature(string name) : base() {
     }
 }
 #endif
