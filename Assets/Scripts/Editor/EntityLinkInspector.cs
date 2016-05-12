6ac61132faff22d96a41f8d0923448a787603d52
Simon Schmid <mail@sschmid.com>
2016-05-12 14:25:06 +0200

    Added EntityLinkInspector

---
+++ Assets/Scripts/Editor/EntityLinkInspector.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using Entitas.Unity.VisualDebugging;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(EntityLink))]
+public class EntityLinkInspector : Editor {
+
+    public override void OnInspectorGUI() {
+        DrawDefaultInspector();
+
+        var link = (EntityLink)target;
+        EditorGUILayout.LabelField(link.entity.ToString());
+
+        if (GUILayout.Button("Show entity")) {
+            Selection.activeGameObject = Object.FindObjectsOfType<EntityBehaviour>()
+                .Single(e => e.entity == link.entity).gameObject;
+        }
+    }
+}
