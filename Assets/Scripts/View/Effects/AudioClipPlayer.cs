ec2e6f10a2403b36b622a7e50ad7c99fe2cf2b85
Simon Schmid <mail@sschmid.com>
2016-09-17 02:32:59 +0200

    Added sounds

---
+++ Assets/Scripts/View/Effects/AudioClipPlayer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class AudioClipPlayer : MonoBehaviour {
+
+    public AudioSource audioSource;
+
+    public float randomizePitch;
+
+    void OnEnable() {
+        audioSource.pitch = GameRandom.view.RandomFloat(1 - randomizePitch, 1 + randomizePitch);
+        audioSource.Play();
+    }
+}
